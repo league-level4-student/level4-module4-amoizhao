9541956fa51a7973d791af4590e11f1a0b910603
zhaoamy <zhaoamy@localhost>
2021-01-30 17:58:09 -0800

    -_-

--- src/_03_polymorphs/MovingMorph.java
+++ src/_03_polymorphs/MovingMorph.java
@@ -3,16 +3,14 @@ package _03_polymorphs;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class BluePolymorph extends Polymorph{
-
-	BluePolymorph(int x, int y) {
+public class MovingMorph extends Polymorph{
+	MovingMorph(int x, int y) {
 		super(x, y);
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(Color.blue);
+		g.setColor(Color.green);
 		g.fillRect(x, y, width, height);
 	}
-	
 }
